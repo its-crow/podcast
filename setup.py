6d4b7b86937f0f323096858d7f4ef2d95c91366a
jack <251087657+its-crow@users.noreply.github.com>
2026-04-11 22:03:25 -0500

    fixed entry point for pip install

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="podgasm",
-    version="0.3",
+    name="podcast",
+    version="0.4",
     packages=["podcast"],
     install_requires=[
         "yt-dlp",
     ],
     entry_points={
         "console_scripts": [
-            "podcast=podcast:main",
+            "podcast=podcast.podcast:main",
         ],
     },
     include_package_data=True,
